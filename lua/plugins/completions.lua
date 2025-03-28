8c6bdc84ae4e39a560de07ff2189ea3b22956520
James Chen <jchen@cpacketnetworks.com>
2025-03-27 17:27:28 -0700

    configure snippet

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -10,25 +10,17 @@ return {
       "rafamadriz/friendly-snippets"
     },
   },
+  { "hrsh7th/cmp-nvim-lsp" },
   {
     "hrsh7th/nvim-cmp",
     config = function()
       local cmp = require 'cmp'
+      require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup({
         snippet = {
           expand = function(args)
-            -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-            -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
-
-            -- For `mini.snippets` users:
-            -- local insert = MiniSnippets.config.expand.insert or MiniSnippets.default_insert
-            -- insert({ body = args.body }) -- Insert at cursor
-            -- cmp.resubscribe({ "TextChangedI", "TextChangedP" })
-            -- require("cmp.config").set_onetime({ sources = {} })
+            require('luasnip').lsp_expand(args.body)
           end
         },
         window = {
@@ -45,9 +37,9 @@ return {
           }) -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         }),
         sources = cmp.config.sources({
-          -- { name = 'nvim_lsp' },
+          { name = 'nvim_lsp' },
           -- { name = 'vsnip' }, -- For vsnip users.
-          -- { name = 'luasnip' }, -- For luasnip users.
+          { name = 'luasnip' }, -- For luasnip users.
           -- { name = 'ultisnips' }, -- For ultisnips users.
           -- { name = 'snippy' }, -- For snippy users.
         }, { {
@@ -88,10 +80,11 @@ return {
       })
 
       -- Set up lspconfig.
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-      require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
-        capabilities = capabilities
-      }
+      -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      -- -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+      -- require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
+      --   capabilities = capabilities
+      -- }
     end
-  } }
+  },
+}
