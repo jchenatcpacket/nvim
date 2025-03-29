34cef6bc755c641c1031057cf9763f5a5a1cb363
James Chen <chenhao540@gmail.com>
2025-03-28 20:54:32 -0700

    use flash nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,6 +13,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    lazy = false,
     config = function()
       require("nvim-treesitter.configs").setup({
         auto_install = true,
@@ -53,9 +54,7 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    options = {
-      theme = "dracula",
-    },
+    lazy = false,
     config = function()
       require('lualine').setup {
       options = {
@@ -103,25 +102,56 @@ return {
   {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
-    ---@module "ibl"
-    ---@type ibl.config
-    opts = {},
+    lazy = false,
     config = function()
       require("ibl").setup()
     end,
   },
   {
     "numToStr/Comment.nvim",
-    opts = {},
+    lazy = false,
     config = function()
       require("Comment").setup()
     end,
   },
   {
     'windwp/nvim-autopairs',
+    lazy = false,
     event = "InsertEnter",
-    config = true
-    -- use opts = {} for passing setup options
-    -- this is equivalent to setup({}) function
-  }
+    config = true,
+    opts = {
+      enabled = function(bufnr) return true end, -- control if auto-pairs should be enabled when attaching to a buffer
+      disable_filetype = { "TelescopePrompt", "spectre_panel", "snacks_picker_input" },
+      disable_in_macro = true, -- disable when recording or executing a macro
+      disable_in_visualblock = false, -- disable when insert after visual block mode
+      disable_in_replace_mode = true,
+      ignored_next_char = [=[[%w%%%'%[%"%.%`%$]]=],
+      enable_moveright = true,
+      enable_afterquote = true, -- add bracket pairs after quote
+      enable_check_bracket_line = true, --- check bracket in same line
+      enable_bracket_in_quote = true, --
+      enable_abbr = false, -- trigger abbreviation
+      break_undo = true, -- switch for basic rule break undo sequence
+      check_ts = false,
+      map_cr = true,
+      map_bs = true, -- map the <BS> key
+      map_c_h = false, -- Map the <C-h> key to delete a pair
+      map_c_w = false, -- map <c-w> to delete a pair if possible
+    },
+  },
+  {
+    "folke/flash.nvim",
+    lazy = false,
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
 }
