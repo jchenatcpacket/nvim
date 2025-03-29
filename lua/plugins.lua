78d8d9913b688d0cc978039d079bb37066624599
James Chen <chenhao540@gmail.com>
2025-03-28 21:25:02 -0700

    material icon

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -49,6 +49,10 @@ return {
             visible = true,
             hide_dotfiles = false,
             hide_gitignored = true,
+            hide_by_name = {
+              ".DS_Store",
+            },
+            never_show = { ".git" },
           },
         }
       }
@@ -156,5 +160,30 @@ return {
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
+  },
+  {
+    'DaikyXendo/nvim-material-icon',
+    lazy = false,
+    config = function()
+      require'nvim-web-devicons'.setup {
+        -- your personnal icons can go here (to override)
+        -- you can specify color or cterm_color instead of specifying both of them
+        -- DevIcon will be appended to `name`
+        override = {
+          zsh = {
+            icon = "",
+            color = "#428850",
+            cterm_color = "65",
+            name = "Zsh"
+          }
+        };
+        -- globally enable different highlight colors per icon (default to true)
+        -- if set to false all icons will have the default icon's color
+        color_icons = true;
+        -- globally enable default icons (default to false)
+        -- will get overriden by `get_icons` option
+        default = true;
+      }
+    end
   },
 }
