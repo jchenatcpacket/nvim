a46fcee7cbc21b3adc78c5b2f5eb2eee435994bf
James Chen <chenhao540@gmail.com>
2025-04-03 21:27:31 -0700

    vscode set up

--- init.lua
+++ init.lua
@@ -55,4 +55,27 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+if vim.g.vscode then
+  require("lazy").setup({
+    spec = {
+      {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        ---@type Flash.Config
+        opts = {},
+        -- stylua: ignore
+        keys = {
+          { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+          { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+          { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+          { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+          { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+        },
+      },
+    },
+    install = { colorscheme = { "habamax" } },
+    checker = { enabled = true },
+  })
+else
+  require("lazy").setup("plugins")
+end
