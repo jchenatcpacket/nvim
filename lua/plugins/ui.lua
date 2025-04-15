6a853adbde04f7479437b41badac2f2424d3eb08
James Chen <chenhao540@gmail.com>
2025-04-14 21:10:18 -0700

    confi neo tree

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -91,9 +91,9 @@ return {
         filesystem = {
           filtered_items = {
             visible = false, -- when true, they will just be displayed differently than normal items
-            hide_dotfiles = true,
-            hide_gitignored = true,
-            hide_hidden = true, -- only works on Windows for hidden files/directories
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            hide_hidden = false, -- only works on Windows for hidden files/directories
             hide_by_name = {
               --"node_modules"
             },
@@ -172,6 +172,7 @@ return {
   {
     'DaikyXendo/nvim-material-icon',
     lazy = false,
+    priority = 900,
     config = function()
       require'nvim-web-devicons'.setup {
         -- your personnal icons can go here (to override)
@@ -196,6 +197,7 @@ return {
   },
   {
     "folke/trouble.nvim",
+    lazy = false,
     opts = {}, -- for default options, refer to the configuration section for custom setup.
     cmd = "Trouble",
     keys = {
