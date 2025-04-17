f39620a10b96b007df388cc25ce585e56b9db646
James Chen <chenhao540@gmail.com>
2025-04-16 22:48:19 -0700

    fs plugins

---
+++ lua/plugins/fs.lua
@@ -0,0 +1,68 @@
+return {
+  {
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+		lazy = false,
+		config = function()
+			require("neo-tree").setup({
+        enable_git_status = true,
+				enable_diagnostics = true,
+        window = {
+          position = "float",
+        },
+				filesystem = {
+					filtered_items = {
+						visible = false, -- when true, they will just be displayed differently than normal items
+						hide_dotfiles = false,
+						hide_gitignored = false,
+						hide_hidden = false, -- only works on Windows for hidden files/directories
+						hide_by_name = {
+							--"node_modules"
+						},
+						hide_by_pattern = { -- uses glob style patterns
+							--"*.meta",
+							--"*/src/*/tsconfig.json",
+						},
+						always_show = { -- remains visible even if other settings would normally hide it
+							".gitignored",
+						},
+						always_show_by_pattern = { -- uses glob style patterns
+							".env*",
+						},
+						never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+							".DS_Store",
+							".git",
+						},
+						never_show_by_pattern = { -- uses glob style patterns
+							--".null-ls_*",
+						},
+					},
+				},
+			})
+
+			vim.keymap.set("n", "<leader>nt", "<Cmd>Neotree toggle<CR>")
+		end,
+	},
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false,
+    config = function ()
+      require("oil").setup({
+        keymaps = {
+          ["<C-c>"] = { "actions.close", mode = "n" },
+        },
+      })
+      vim.keymap.set("n", "<leader>oil", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    end
+  },
+}
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -115,51 +115,6 @@ return {
 		},
 	},
 	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-		},
-		lazy = false,
-		config = function()
-			require("neo-tree").setup({
-				enable_diagnostics = false,
-				filesystem = {
-					filtered_items = {
-						visible = false, -- when true, they will just be displayed differently than normal items
-						hide_dotfiles = false,
-						hide_gitignored = false,
-						hide_hidden = false, -- only works on Windows for hidden files/directories
-						hide_by_name = {
-							--"node_modules"
-						},
-						hide_by_pattern = { -- uses glob style patterns
-							--"*.meta",
-							--"*/src/*/tsconfig.json",
-						},
-						always_show = { -- remains visible even if other settings would normally hide it
-							".gitignored",
-						},
-						always_show_by_pattern = { -- uses glob style patterns
-							".env*",
-						},
-						never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
-							".DS_Store",
-							".git",
-						},
-						never_show_by_pattern = { -- uses glob style patterns
-							--".null-ls_*",
-						},
-					},
-				},
-			})
-
-			vim.keymap.set("n", "<leader>e", "<Cmd>Neotree toggle<CR>")
-		end,
-	},
-	{
 		"DaikyXendo/nvim-material-icon",
 		lazy = false,
 		priority = 900,
