d188e575f4bad546ae939b9de03c568ed16f223f
James Chen <jchen@cpacketnetworks.com>
2025-04-17 13:23:24 -0700

    keymap

--- lua/plugins/fuzzy.lua
+++ lua/plugins/fuzzy.lua
@@ -1,14 +1,14 @@
 return {
-	"ibhagwan/fzf-lua",
-	-- optional for icon support
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	-- or if using mini.icons/mini.nvim
-	-- dependencies = { "echasnovski/mini.icons" },
-	opts = {},
-	config = function()
-		require("fzf-lua").setup()
-    vim.keymap.set("n", "<C-p>", "<CMD>FzfLua files<CR>", { desc = "search filenames" })
-    vim.keymap.set("n", "<C-f>b", "<CMD>FzfLua grep_curbuf<CR>", { desc = "search current buffer" })
-	end,
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    -- or if using mini.icons/mini.nvim
+    -- dependencies = { "echasnovski/mini.icons" },
+    opts = {},
+    config = function()
+        require("fzf-lua").setup()
+        vim.keymap.set("n", "<C-p>", "<CMD>FzfLua files<CR>", { desc = "search filenames" })
+        vim.keymap.set("n", "<C-o>", "<CMD>FzfLua grep_curbuf<CR>", { desc = "search current buffer" })
+        vim.keymap.set("n", "<leader>gall", "<CMD>FzfLua grep_project<CR>", { desc = "search all project lines" })
+    end,
 }
-
