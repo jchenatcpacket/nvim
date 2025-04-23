1b0ee1243c8d3297e56624e38547ad3a91082595
James Chen <jchen@cpacketnetworks.com>
2025-04-23 13:39:09 -0700

    fm all files

--- init.lua
+++ init.lua
@@ -8,28 +8,28 @@ vim.cmd("set shiftwidth=4")
 vim.cmd("set list")
 
 -- custom function to setting indent to 2 or 4
-vim.api.nvim_create_user_command('SetIndent', function(opts)
-    local width = tonumber(opts.args)
+vim.api.nvim_create_user_command("SetIndent", function(opts)
+	local width = tonumber(opts.args)
 
-    if width ~= 2 and width ~= 4 then
-        vim.api.nvim_err_writeln("Error: Indent must be either 2 or 4")
-        return
-    end
+	if width ~= 2 and width ~= 4 then
+		vim.api.nvim_err_writeln("Error: Indent must be either 2 or 4")
+		return
+	end
 
-    if width == 2 then
-        vim.cmd([[set listchars=trail:…,nbsp:+,tab:│\ ,leadmultispace:▏\ ,multispace:┊\ ,eol:↵]])
-    elseif width == 4 then
-        vim.cmd([[set listchars=trail:…,nbsp:+,tab:│\ ,leadmultispace:▏\ \ \ ,multispace:┊\ ,eol:↵]])
-    end
+	if width == 2 then
+		vim.cmd([[set listchars=trail:…,nbsp:+,tab:│\ ,leadmultispace:▏\ ,multispace:┊\ ,eol:↵]])
+	elseif width == 4 then
+		vim.cmd([[set listchars=trail:…,nbsp:+,tab:│\ ,leadmultispace:▏\ \ \ ,multispace:┊\ ,eol:↵]])
+	end
 
-    vim.opt.tabstop = width
-    vim.opt.softtabstop = width
-    vim.opt.shiftwidth = width
+	vim.opt.tabstop = width
+	vim.opt.softtabstop = width
+	vim.opt.shiftwidth = width
 
-    print("Indent set to " .. width)
+	print("Indent set to " .. width)
 end, { nargs = 1 })
 
-vim.api.nvim_create_autocmd("BufEnter", { command = "SetIndent 4", })
+vim.api.nvim_create_autocmd("BufEnter", { command = "SetIndent 4" })
 
 vim.opt.wrap = true
 vim.opt.hlsearch = false
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,207 +1,207 @@
 return {
-    {
-        "akinsho/bufferline.nvim",
-        version = "*",
-        lazy = false,
-        dependencies = "nvim-tree/nvim-web-devicons",
-        config = function()
-            vim.opt.termguicolors = true
-            require("bufferline").setup({})
-        end,
-    },
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-            "SmiteshP/nvim-navic",
-        },
-        lazy = false,
-        config = function()
-            require("lualine").setup({
-                options = {
-                    icons_enabled = true,
-                    theme = "auto",
-                    component_separators = { left = "", right = "" },
-                    section_separators = { left = "", right = "" },
-                    disabled_filetypes = {
-                        statusline = {},
-                        winbar = {},
-                    },
-                    ignore_focus = {},
-                    always_divide_middle = true,
-                    always_show_tabline = true,
-                    globalstatus = false,
-                    refresh = {
-                        statusline = 100,
-                        tabline = 100,
-                        winbar = 100,
-                    },
-                },
-                sections = {
-                    lualine_a = { "mode" },
-                    lualine_b = { "branch", "diff" },
-                    lualine_c = {
-                        {
-                            "filename",
-                            path = 3,
-                        },
-                        {
-                            "navic",
-                            color_correction = "dynamic",
-                            navic_opts = nil,
-                        }
-                    },
-                    lualine_x = {
-                        {
-                            "diagnostics",
-                            -- Table of diagnostic sources, available sources are:
-                            --   'nvim_lsp', 'nvim_diagnostic', 'nvim_workspace_diagnostic', 'coc', 'ale', 'vim_lsp'.
-                            -- or a function that returns a table as such:
-                            --   { error=error_cnt, warn=warn_cnt, info=info_cnt, hint=hint_cnt }
-                            sources = { "nvim_diagnostic", "coc" },
+	{
+		"akinsho/bufferline.nvim",
+		version = "*",
+		lazy = false,
+		dependencies = "nvim-tree/nvim-web-devicons",
+		config = function()
+			vim.opt.termguicolors = true
+			require("bufferline").setup({})
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+			"SmiteshP/nvim-navic",
+		},
+		lazy = false,
+		config = function()
+			require("lualine").setup({
+				options = {
+					icons_enabled = true,
+					theme = "auto",
+					component_separators = { left = "", right = "" },
+					section_separators = { left = "", right = "" },
+					disabled_filetypes = {
+						statusline = {},
+						winbar = {},
+					},
+					ignore_focus = {},
+					always_divide_middle = true,
+					always_show_tabline = true,
+					globalstatus = false,
+					refresh = {
+						statusline = 100,
+						tabline = 100,
+						winbar = 100,
+					},
+				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch", "diff" },
+					lualine_c = {
+						{
+							"filename",
+							path = 3,
+						},
+						{
+							"navic",
+							color_correction = "dynamic",
+							navic_opts = nil,
+						},
+					},
+					lualine_x = {
+						{
+							"diagnostics",
+							-- Table of diagnostic sources, available sources are:
+							--   'nvim_lsp', 'nvim_diagnostic', 'nvim_workspace_diagnostic', 'coc', 'ale', 'vim_lsp'.
+							-- or a function that returns a table as such:
+							--   { error=error_cnt, warn=warn_cnt, info=info_cnt, hint=hint_cnt }
+							sources = { "nvim_diagnostic", "coc" },
 
-                            -- Displays diagnostics for the defined severity types
-                            sections = { "error", "warn", "info", "hint" },
+							-- Displays diagnostics for the defined severity types
+							sections = { "error", "warn", "info", "hint" },
 
-                            diagnostics_color = {
-                                -- Same values as the general color option can be used here.
-                                error = "DiagnosticError", -- Changes diagnostics' error color.
-                                warn = "DiagnosticWarn", -- Changes diagnostics' warn color.
-                                info = "DiagnosticInfo", -- Changes diagnostics' info color.
-                                hint = "DiagnosticHint", -- Changes diagnostics' hint color.
-                            },
-                            symbols = { error = "E", warn = "W", info = "I", hint = "H" },
-                            colored = true, -- Displays diagnostics status in color if set to true.
-                            update_in_insert = false, -- Update diagnostics in insert mode.
-                            always_visible = false, -- Show diagnostics even if there are none.
-                        },
-                        {
-                            "lsp_status",
-                            icon = "", -- f013
-                            symbols = {
-                                -- Standard unicode symbols to cycle through for LSP progress:
-                                spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
-                                -- Standard unicode symbol for when LSP is done:
-                                done = "✓",
-                                -- Delimiter inserted between LSP names:
-                                separator = " ",
-                            },
-                            -- List of LSP names to ignore (e.g., `null-ls`):
-                            ignore_lsp = {},
-                        },
-                    },
-                    lualine_y = { "location", "progress", "encoding", "filetype" },
-                    lualine_z = {},
-                },
-                inactive_sections = {
-                    lualine_a = {},
-                    lualine_b = {},
-                    lualine_c = {},
-                    lualine_x = {},
-                    lualine_y = {},
-                    lualine_z = {},
-                },
-                tabline = {},
-                winbar = {},
-                inactive_winbar = {},
-                extensions = {},
-            })
-        end,
-    },
-    {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        lazy = false,
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        },
-        keys = {
-            {
-                "<leader>?",
-                function()
-                    require("which-key").show({ global = false })
-                end,
-                desc = "Buffer Local Keymaps (which-key)",
-            },
-        },
-    },
-    {
-        "DaikyXendo/nvim-material-icon",
-        lazy = false,
-        priority = 900,
-        config = function()
-            require("nvim-web-devicons").setup({
-                -- your personnal icons can go here (to override)
-                -- you can specify color or cterm_color instead of specifying both of them
-                -- DevIcon will be appended to `name`
-                override = {
-                    zsh = {
-                        icon = "",
-                        color = "#428850",
-                        cterm_color = "65",
-                        name = "Zsh",
-                    },
-                },
-                -- globally enable different highlight colors per icon (default to true)
-                -- if set to false all icons will have the default icon's color
-                color_icons = true,
-                -- globally enable default icons (default to false)
-                -- will get overriden by `get_icons` option
-                default = true,
-            })
-        end,
-    },
-    {
-        "SmiteshP/nvim-navic",
-        lazy = false,
-        config = function ()
-            local navic = require("nvim-navic")
-            navic.setup {
-                icons = {
-                    File          = "󰈙 ",
-                    Module        = " ",
-                    Namespace     = "󰌗 ",
-                    Package       = " ",
-                    Class         = "󰌗 ",
-                    Method        = "󰆧 ",
-                    Property      = " ",
-                    Field         = " ",
-                    Constructor   = " ",
-                    Enum          = "󰕘",
-                    Interface     = "󰕘",
-                    Function      = "󰊕 ",
-                    Variable      = "󰆧 ",
-                    Constant      = "󰏿 ",
-                    String        = "󰀬 ",
-                    Number        = "󰎠 ",
-                    Boolean       = "◩ ",
-                    Array         = "󰅪 ",
-                    Object        = "󰅩 ",
-                    Key           = "󰌋 ",
-                    Null          = "󰟢 ",
-                    EnumMember    = " ",
-                    Struct        = "󰌗 ",
-                    Event         = " ",
-                    Operator      = "󰆕 ",
-                    TypeParameter = "󰊄 ",
-                },
-                lsp = {
-                    auto_attach = true,
-                    preference = nil,
-                },
-                highlight = true,
-                separator = " > ",
-                depth_limit = 0,
-                depth_limit_indicator = "..",
-                safe_output = true,
-                lazy_update_context = false,
-                click = false,
-                format_text = function(text)
-                    return text
-                end,
-            }
-        end
-    }
+							diagnostics_color = {
+								-- Same values as the general color option can be used here.
+								error = "DiagnosticError", -- Changes diagnostics' error color.
+								warn = "DiagnosticWarn", -- Changes diagnostics' warn color.
+								info = "DiagnosticInfo", -- Changes diagnostics' info color.
+								hint = "DiagnosticHint", -- Changes diagnostics' hint color.
+							},
+							symbols = { error = "E", warn = "W", info = "I", hint = "H" },
+							colored = true, -- Displays diagnostics status in color if set to true.
+							update_in_insert = false, -- Update diagnostics in insert mode.
+							always_visible = false, -- Show diagnostics even if there are none.
+						},
+						{
+							"lsp_status",
+							icon = "", -- f013
+							symbols = {
+								-- Standard unicode symbols to cycle through for LSP progress:
+								spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
+								-- Standard unicode symbol for when LSP is done:
+								done = "✓",
+								-- Delimiter inserted between LSP names:
+								separator = " ",
+							},
+							-- List of LSP names to ignore (e.g., `null-ls`):
+							ignore_lsp = {},
+						},
+					},
+					lualine_y = { "location", "progress", "encoding", "filetype" },
+					lualine_z = {},
+				},
+				inactive_sections = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = {},
+					lualine_x = {},
+					lualine_y = {},
+					lualine_z = {},
+				},
+				tabline = {},
+				winbar = {},
+				inactive_winbar = {},
+				extensions = {},
+			})
+		end,
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		lazy = false,
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
+	{
+		"DaikyXendo/nvim-material-icon",
+		lazy = false,
+		priority = 900,
+		config = function()
+			require("nvim-web-devicons").setup({
+				-- your personnal icons can go here (to override)
+				-- you can specify color or cterm_color instead of specifying both of them
+				-- DevIcon will be appended to `name`
+				override = {
+					zsh = {
+						icon = "",
+						color = "#428850",
+						cterm_color = "65",
+						name = "Zsh",
+					},
+				},
+				-- globally enable different highlight colors per icon (default to true)
+				-- if set to false all icons will have the default icon's color
+				color_icons = true,
+				-- globally enable default icons (default to false)
+				-- will get overriden by `get_icons` option
+				default = true,
+			})
+		end,
+	},
+	{
+		"SmiteshP/nvim-navic",
+		lazy = false,
+		config = function()
+			local navic = require("nvim-navic")
+			navic.setup({
+				icons = {
+					File = "󰈙 ",
+					Module = " ",
+					Namespace = "󰌗 ",
+					Package = " ",
+					Class = "󰌗 ",
+					Method = "󰆧 ",
+					Property = " ",
+					Field = " ",
+					Constructor = " ",
+					Enum = "󰕘",
+					Interface = "󰕘",
+					Function = "󰊕 ",
+					Variable = "󰆧 ",
+					Constant = "󰏿 ",
+					String = "󰀬 ",
+					Number = "󰎠 ",
+					Boolean = "◩ ",
+					Array = "󰅪 ",
+					Object = "󰅩 ",
+					Key = "󰌋 ",
+					Null = "󰟢 ",
+					EnumMember = " ",
+					Struct = "󰌗 ",
+					Event = " ",
+					Operator = "󰆕 ",
+					TypeParameter = "󰊄 ",
+				},
+				lsp = {
+					auto_attach = true,
+					preference = nil,
+				},
+				highlight = true,
+				separator = " > ",
+				depth_limit = 0,
+				depth_limit_indicator = "..",
+				safe_output = true,
+				lazy_update_context = false,
+				click = false,
+				format_text = function(text)
+					return text
+				end,
+			})
+		end,
+	},
 }
