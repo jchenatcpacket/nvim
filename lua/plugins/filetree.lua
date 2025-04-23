dee2a60649712a39da55594cd93f04f21c9d9dba
James Chen <jchen@cpacketnetworks.com>
2025-04-23 13:34:17 -0700

    fm file

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -15,24 +15,24 @@ return {
 				window = {
 					position = "float",
 					mappings = {
-            ["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
-            -- Read `# Preview Mode` for more information
-            ["l"] = "focus_preview",
-            ["af"] = {
-              "add",
-              -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
-              -- some commands may take optional config options, see `:h neo-tree-mappings` for details
-              config = {
-                show_path = "none", -- "none", "relative", "absolute"
-              },
-            },
+						["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
+						-- Read `# Preview Mode` for more information
+						["l"] = "focus_preview",
+						["af"] = {
+							"add",
+							-- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
+							-- some commands may take optional config options, see `:h neo-tree-mappings` for details
+							config = {
+								show_path = "none", -- "none", "relative", "absolute"
+							},
+						},
 						["ad"] = "add_directory",
-            ["m"] = "move",
+						["m"] = "move",
 						["d"] = "delete",
 						["r"] = "rename_basename",
-            ["q"] = "cancel",
-            ["s"] = "open_vsplit",
-            ["t"] = "open_tabnew",
+						["q"] = "cancel",
+						["s"] = "open_vsplit",
+						["t"] = "open_tabnew",
 					},
 				},
 				filesystem = {
