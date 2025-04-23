10d1783bcefbbe3add4947589bb844e7fc9eb33c
James Chen <jchen@cpacketnetworks.com>
2025-04-23 16:14:06 -0700

    cus git status column

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,9 +1,25 @@
 return {
-    {
+	{
 		"lewis6991/gitsigns.nvim",
 		lazy = false,
 		config = function()
 			require("gitsigns").setup({
+				signs = {
+					add = { text = "┃a" },
+					change = { text = "┃C" },
+					delete = { text = "_d" },
+					topdelete = { text = "‾td" },
+					changedelete = { text = "~cd" },
+					untracked = { text = "┆u" },
+				},
+				signs_staged = {
+					add = { text = "┃a" },
+					change = { text = "┃c" },
+					delete = { text = "_" },
+					topdelete = { text = "‾td" },
+					changedelete = { text = "~cd" },
+					untracked = { text = "┆u" },
+				},
 				current_line_blame = false,
 				current_line_blame_opts = {
 					virt_text = true,
@@ -13,7 +29,7 @@ return {
 					virt_text_priority = 100,
 					use_focus = true,
 				},
-				current_line_blame_formatter = '  <author>, <author_time:%R> - <summary>',
+				current_line_blame_formatter = "  <author>, <author_time:%R> - <summary>",
 			})
 		end,
 	},
