941ffcb46af1fa3ad9c5390d01a0736375dccd44
James Chen <chenhao540@gmail.com>
2025-04-27 16:58:07 -0700

    update navic color

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -20,8 +20,12 @@ return {
 			require("lualine").setup({
 				options = {
 					icons_enabled = true,
-					theme = "dracula",
-					component_separators = { left = "", right = "" },
+					theme = "auto",
+					component_separators = { 
+						-- left = "",
+						left = ">",
+						right = "",
+					},
 					section_separators = { left = "", right = "" },
 					disabled_filetypes = {
 						statusline = {},
@@ -161,32 +165,32 @@ return {
 			local navic = require("nvim-navic")
 			navic.setup({
 				icons = {
-					File = "󰈙 ",
-					Module = " ",
-					Namespace = "󰌗 ",
-					Package = " ",
-					Class = "󰌗 ",
-					Method = "󰆧 ",
-					Property = " ",
-					Field = " ",
-					Constructor = " ",
-					Enum = "󰕘",
-					Interface = "󰕘",
-					Function = "󰊕 ",
-					Variable = "󰆧 ",
-					Constant = "󰏿 ",
-					String = "󰀬 ",
-					Number = "󰎠 ",
-					Boolean = "◩ ",
-					Array = "󰅪 ",
-					Object = "󰅩 ",
-					Key = "󰌋 ",
-					Null = "󰟢 ",
-					EnumMember = " ",
-					Struct = "󰌗 ",
-					Event = " ",
-					Operator = "󰆕 ",
-					TypeParameter = "󰊄 ",
+					File = ' ',
+					Module = ' ',
+					Namespace = ' ',
+					Package = ' ',
+					Class = ' ',
+					Method = ' ',
+					Property = ' ',
+					Field = ' ',
+					Constructor = ' ',
+					Enum = ' ',
+					Interface = ' ',
+					Function = ' ',
+					Variable = ' ',
+					Constant = ' ',
+					String = ' ',
+					Number = ' ',
+					Boolean = ' ',
+					Array = ' ',
+					Object = ' ',
+					Key = ' ',
+					Null = ' ',
+					EnumMember = ' ',
+					Struct = ' ',
+					Event = ' ',
+					Operator = ' ',
+					TypeParameter = ' '
 				},
 				lsp = {
 					auto_attach = true,
@@ -200,6 +204,35 @@ return {
 				lazy_update_context = false,
 				click = true,
 			})
+
+			vim.api.nvim_set_hl(0, "NavicIconsFile",          {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicIconsModule",        {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicIconsNamespace",     {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicIconsPackage",       {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicIconsClass",         {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicIconsMethod",        {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicIconsProperty",      {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicIconsField",         {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicIconsConstructor",   {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicIconsEnum",          {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicIconsInterface",     {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicIconsFunction",      {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicIconsVariable",      {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicIconsConstant",      {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicIconsString",        {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicIconsNumber",        {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicIconsBoolean",       {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicIconsArray",         {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicIconsObject",        {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicIconsKey",           {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicIconsNull",          {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicIconsEnumMember",    {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicIconsStruct",        {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicIconsEvent",         {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicIconsOperator",      {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicIconsTypeParameter", {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicText",               {default = true, bg = "#000000", fg = "#ffffff"})
+			vim.api.nvim_set_hl(0, "NavicSeparator",          {default = true, bg = "#000000", fg = "#ffffff"})
 		end,
 	},
 }
