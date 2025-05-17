f7d641f5e489b2c0820bfc3490b064f99c44c63c
James Chen <chenhao540@gmail.com>
2025-05-17 16:04:39 -0700

    update menu

--- README.md
+++ README.md
@@ -1,12 +1,5 @@
 # my neovim setup
 
-## Basics
-- search files, buffer, project lines...
-- neotree, lualine, bufferline
-- flash.nvim
-- treesitter, lsp, linter, formatter, NO DAP (Debug adapter protocol)
-- snippets/completions
-- gitsigns for preview hunk, revert hunk/changes
-
-## To-Add in the future
-- LSP from docker container
+## Todo
+fix MyMenu close and reopen erroneous behaviour
+customize right click menu using nui for lsp behaviour, find def, ref, goto def
--- lua/mymenu.lua
+++ lua/mymenu.lua
@@ -18,50 +18,60 @@ local callback = function(selection)
   end
 end
 
-local menu = Menu(
-  {
-    position = "50%",
-    size = {
-      width = 25,
-      height = 5,
-    },
-    border = {
-      style = "single",
-      text = {
-        top = "MyMenu",
-        top_align = "center",
-      },
-    },
-    win_options = {
-      winhighlight = "Normal:Normal,FloatBorder:Normal",
+local popup_options = {
+  relative = "win",
+  position = {
+    row = "55%",
+    col = "33%"
+  },
+  size = {
+    width = 40,
+    height = 20,
+  },
+  border = {
+    style = "rounded",
+    text = {
+      top = "MyMenu",
+      top_align = "center",
     },
   },
-  {
-    lines = {
-      Menu.separator("Fzf"),
-      Menu.item("grep project"),
-      Menu.item("grep current buffer"),
-      Menu.item("find files"),
-      Menu.item("find lsp workspace symbols"),
-      Menu.item("find lsp document symbols"),
-    },
-    max_width = 20,
-    keymap = {
-      focus_next = { "j", "<Down>", "<Tab>" },
-      focus_prev = { "k", "<Up>", "<S-Tab>" },
-      close = { "<Esc>", "<C-c>" },
-      submit = { "<CR>", "<Space>" },
-    },
-    on_close = function()
-      print("MyMenu Closed!")
-    end,
-    on_submit = function(item)
-      local selection = item.text
-      callback(selection)
-    end,
-  }
-)
+  win_options = {
+    winhighlight = "Normal:Normal,FloatBorder:Normal",
+  },
+  focusable = true
+}
+
+local menu = Menu(popup_options, {
+  lines = {
+    Menu.separator("Fzf"),
+    Menu.item("grep project", { id = 1 }),
+    Menu.item("grep current buffer", { id = 2 }),
+    Menu.item("find files", { id = 3 }),
+    Menu.item("find lsp workspace symbols", { id = 4 }),
+    Menu.item("find lsp document symbols", { id = 5 }),
+  },
+  -- max_width = 20,
+  keymap = {
+    focus_next = { "j", "<Down>" },
+    focus_prev = { "k", "<Up>" },
+    close = { "<Esc>" },
+    submit = { "<CR>" },
+  },
+
+  on_close = function()
+    print("MyMenu Closed!")
+  end,
+
+  on_submit = function(item)
+    local selection = item.text
+    callback(selection)
+  end,
+})
 
 vim.keymap.set("n", "<leader>m", function()
   menu:mount()
 end, { desc = "show MyMenu" })
+
+menu:on({ event.BufLeave }, function()
+  menu:unmount()
+end, { once = true })
