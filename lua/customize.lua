c3fec4942edd506bc8071dd5472511099d289276
James Chen <chenhao540@gmail.com>
2025-05-17 21:29:45 -0700

    add folding

--- lua/customize.lua
+++ lua/customize.lua
@@ -43,3 +43,25 @@ vim.cmd("set cursorline")
 vim.cmd("set clipboard=unnamedplus")
 
 vim.opt.termguicolors = true
+
+-- folding
+vim.o.foldenable = true
+vim.o.foldlevel = 99
+vim.o.foldmethod = "expr"
+vim.o.foldtext = ""
+vim.opt.foldcolumn = "0"
+vim.opt.fillchars:append({ fold = " " })
+
+-- default to tressitter folding
+vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+-- Prefer LSP folding if client supports it
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client:supports_method('textDocument/foldingRange') then
+      local win = vim.api.nvim_get_current_win()
+      vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
+    end
+  end,
+})
