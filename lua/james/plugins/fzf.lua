f7b15d901ea5c256fbbfa1b8540a4f295f5382f2
James Chen <jchen@cpacketnetworks.com>
2025-10-30 16:36:07 -0700

    more plugins

--- lua/james/plugins/fzf.lua
+++ lua/james/plugins/fzf.lua
@@ -2,4 +2,13 @@ return {
   "ibhagwan/fzf-lua",
   dependencies = { "DaikyXendo/nvim-material-icon" },
   opts = {},
+  config = function ()
+    -- vim.keymap.set({ "n", "v", "i" }, "<C-f>",
+    -- function() FzfLua.grep_curbuf() end,
+    -- { silent = true, desc = "search current buffer" })
+
+    -- vim.keymap.set({ "n", "v", "i" }, "<C-p>",
+    -- function() FzfLua.grep_project() end,
+    -- { silent = true, desc = "find files" })
+  end
 }
