7034683283d8fb049699209e3c99477588ef1af6
James Chen <jchen@cpacketnetworks.com>
2025-10-30 14:12:32 -0700

    rework

--- init.lua
+++ init.lua
@@ -1,12 +1,6 @@
 if vim.g.vscode then
     -- VSCode extension
 else
-    -- ordinary Neovim
-    -- require('customize')
-    -- require('keymaps')
-    -- require('mymenu')
-    -- require("old.lazy")
     require('common')
     require("james.lazy")
-    -- require("cclear")
 end
---
+++ lua/james/plugins/conform.lua
@@ -0,0 +1,31 @@
+return {
+  "stevearc/conform.nvim",
+  dependencies = { "mason.nvim" },
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+    conform.setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        -- Conform will run multiple formatters sequentially
+        python = { "isort", "black" },
+        -- You can customize some of the format options for the filetype (:help conform.format)
+        rust = { "rustfmt", lsp_format = "fallback" },
+        -- Conform will run the first available formatter
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+    })
+
+    vim.keymap.set({ "n" }, "<leader>fm", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500,
+      })
+    end, { desc = "Format file or a range of lines" })
+
+    vim.keymap.set({ "n" }, "<leader>t", function()
+      conform.format({ formatters = { "trim_whitespace" } })
+    end, { desc = "trim trailing whitespace" })
+  end,
+}
