44c3e425665358c9d9dbb79126b0a0dcd0ba2832
James Chen <jchen@cpacketnetworks.com>
2025-10-30 17:12:35 -0700

    more config

--- lua/james/plugins/fzf.lua
+++ lua/james/plugins/fzf.lua
@@ -3,12 +3,13 @@ return {
   dependencies = { "DaikyXendo/nvim-material-icon" },
   opts = {},
   config = function ()
-    -- vim.keymap.set({ "n", "v", "i" }, "<C-f>",
-    -- function() FzfLua.grep_curbuf() end,
-    -- { silent = true, desc = "search current buffer" })
+    local fzf = require("fzf-lua")
+    vim.keymap.set({ "n", "v", "i" }, "<C-f>",
+    function() fzf.grep_curbuf() end,
+    { silent = true, desc = "search current buffer" })
 
-    -- vim.keymap.set({ "n", "v", "i" }, "<C-p>",
-    -- function() FzfLua.grep_project() end,
-    -- { silent = true, desc = "find files" })
+    vim.keymap.set({ "n", "v", "i" }, "<C-p>",
+    function() fzf.files() end,
+    { silent = true, desc = "find files" })
   end
 }
