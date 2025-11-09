aba98c9bff48a9cb28918ebeb5ae3b738ab02d8d
James Chen <chenhao540@gmail.com>
2025-11-08 19:37:24 -0800

    yanky use system clipboard

--- lua/james/plugins/yanky.lua
+++ lua/james/plugins/yanky.lua
@@ -26,6 +26,9 @@ return {
             preserve_cursor_position = {
                 enabled = true,
             },
+            system_clipboard = {
+                sync_with_ring = true,
+            },
         })
     end
 }
