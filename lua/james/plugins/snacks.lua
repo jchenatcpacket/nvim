4ccf4af771fa2f9eb1ed5017ef199e43226380d8
James Chen <chenhao540@gmail.com>
2025-11-09 12:00:21 -0800

    upgrade yanky

--- lua/james/plugins/snacks.lua
+++ lua/james/plugins/snacks.lua
@@ -7,18 +7,18 @@ return {
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
-        -- bigfile = { enabled = true },
-        -- dashboard = { enabled = true },
-        -- explorer = { enabled = true },
+        bigfile = { enabled = true },
+        dashboard = { enabled = false },
+        explorer = { enabled = false },
         indent = { enabled = true },
-        -- input = { enabled = true },
+        input = { enabled = false },
         picker = { enabled = true },
-        -- notifier = { enabled = true },
-        -- quickfile = { enabled = true },
-        -- scope = { enabled = true },
+        notifier = { enabled = false },
+        quickfile = { enabled = false },
+        scope = { enabled = false },
         scroll = { enabled = true },
-        -- statuscolumn = { enabled = true },
-        -- words = { enabled = true },
+        statuscolumn = { enabled = false },
+        words = { enabled = false },
 
         lazygit = {
             -- your lazygit configuration comes here
@@ -33,6 +33,6 @@ return {
     config = function()
         vim.api.nvim_create_user_command('Lazygit', function()
             Snacks.lazygit()
-        end, {})
+        end, { desc = "lazygit" })
     end
 }
