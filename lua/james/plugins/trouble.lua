a71dfd1e28d2979ed36479024f4f428a42e7ed04
James Chen <jchen@cpacketnetworks.com>
2025-11-10 15:29:39 -0800

    more setup change

--- lua/james/plugins/trouble.lua
+++ lua/james/plugins/trouble.lua
@@ -1,15 +1,32 @@
 return {
   "folke/trouble.nvim",
   opts = {
-    {
-      focus = false,
-      follow = true,
-      win = {
-        position = "right",
-      }
+    modes = {
+      symbols = {
+        focus = false,
+        follow = true,
+        win = {
+          type = "split",     -- split window
+          relative = "win",   -- relative to current window
+          position = "right", -- right side
+          size = 0.4
+          ,                   -- 30% of the window
+        },
+      },
     }
   }, -- for default options, refer to the configuration section for custom setup.
   cmd = "Trouble",
-  keys = {},
+  keys = {
+    {
+      "<leader>td",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>ts",
+      "<cmd>Trouble symbols toggle<cr>",
+      desc = "Symbols (Trouble)",
+    },
+  },
   cond = not vim.g.vscode,
 }
