dd33b713bfa22d73e7c2c32319e9a5debacdf795
James Chen <jchen@cpacketnetworks.com>
2025-11-13 14:57:30 -0800

    more configs

--- lua/james/plugins/mini.lua
+++ lua/james/plugins/mini.lua
@@ -1,8 +1,16 @@
 return {
-    "nvim-mini/mini.pairs",
-    version = "*",
-    config = function ()
-        require('mini.pairs').setup()
-    end
+    {
+        "nvim-mini/mini.pairs",
+        version = "*",
+        config = function()
+            require('mini.pairs').setup()
+        end
+    },
+    {
+        "nvim-mini/mini.indentscope",
+        version = '*',
+        config = function ()
+            require('mini.indentscope').setup()
+        end
+    }
 }
-
