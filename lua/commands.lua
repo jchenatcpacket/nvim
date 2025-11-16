52019a5a6dbc21226cf604351bdc4a37b2be9820
James Chen <chenhao540@gmail.com>
2025-11-16 14:31:54 -0800

    new command

--- lua/commands.lua
+++ lua/commands.lua
@@ -2,6 +2,10 @@ vim.api.nvim_create_user_command('Lazygit', function()
   Snacks.lazygit()
 end, { desc = "Lazygit" })
 
+vim.api.nvim_create_user_command('Terminal', function()
+  Snacks.terminal()
+end, { desc = "Snacks Terminal" })
+
 vim.api.nvim_create_user_command('Explorer', function()
   Snacks.explorer()
 end, { desc = "File Explorer" })
