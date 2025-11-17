c8f417b95de083f3c202c63decba7eb97b38046d
James Chen <jchen@cpacketnetworks.com>
2025-11-17 15:20:13 -0800

    relocate organize files

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,3 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
 vim.keymap.set("i", "<C-s>", "<cmd>w<cr><Esc>", { desc = "insert mode, save file" })
 vim.keymap.set("n", "<C-s>", "<cmd>w<cr>", { desc = "normal mode, save file" })
 vim.keymap.set("i", "<D-s>", "<cmd>w<cr><Esc>", { desc = "insert mode, save file" })
