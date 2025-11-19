0d9f16cdab83d0d2a6b92ff62063de1231dd1b31
James Chen <chenhao540@gmail.com>
2025-11-19 10:43:22 -0800

    add copilot chat

--- lua/james/plugins/fzf.lua
+++ lua/james/plugins/fzf.lua
@@ -1,16 +1,17 @@
 return {
-  "ibhagwan/fzf-lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {},
-  config = function ()
-    local fzf = require("fzf-lua")
-    vim.keymap.set({ "n", "v", "i" }, "<C-f>",
-    function() fzf.grep_curbuf() end,
-    { silent = true, desc = "search current buffer" })
+	"ibhagwan/fzf-lua",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {},
+	config = function()
+		local fzf = require("fzf-lua")
+		fzf.register_ui_select()
+		vim.keymap.set({ "n", "v", "i" }, "<C-f>", function()
+			fzf.grep_curbuf()
+		end, { silent = true, desc = "search current buffer" })
 
-    vim.keymap.set({ "n", "v", "i" }, "<C-p>",
-    function() fzf.files() end,
-    { silent = true, desc = "find files" })
-  end,
-  cond = not vim.g.vscode,
+		vim.keymap.set({ "n", "v", "i" }, "<C-p>", function()
+			fzf.files()
+		end, { silent = true, desc = "find files" })
+	end,
+	cond = not vim.g.vscode,
 }
