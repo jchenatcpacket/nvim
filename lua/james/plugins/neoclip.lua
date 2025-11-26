dd53bd90ffd24f9f3de6f5b368f73885feeca2bf
James Chen <chenhao540@gmail.com>
2025-11-25 21:22:26 -0800

    add neoclip

---
+++ lua/james/plugins/neoclip.lua
@@ -0,0 +1,11 @@
+return {
+	"AckslD/nvim-neoclip.lua",
+	dependencies = {
+		{ "ibhagwan/fzf-lua" },
+	},
+	config = function()
+		require("neoclip").setup({
+            default_register = '*',
+        })
+	end,
+}
