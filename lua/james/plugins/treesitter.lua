2b1dd83a4428a9cf0cc56514cb5cbfbbcd310472
James Chen <jchen@cpacketnetworks.com>
2025-12-02 15:29:41 -0800

    comment out ts textobject

--- lua/james/plugins/treesitter.lua
+++ lua/james/plugins/treesitter.lua
@@ -19,65 +19,65 @@ return {
 			})
 		end,
 	},
-	{
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		event = "VeryLazy",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				textobjects = {
-					move = {
-						enable = true,
-						set_jumps = true, -- whether to set jumps in the jumplist
-						goto_next_start = {
-							-- ["]m"] = "@function.outer",
-							-- ["gj"] = "@function.outer",
-							-- ["]]"] = "@class.outer",
-							-- ["]b"] = "@block.outer",
-							-- ["]a"] = "@parameter.inner",
-						},
-						goto_next_end = {
-							-- ["]M"] = "@function.outer",
-							-- ["gJ"] = "@function.outer",
-							-- ["]["] = "@class.outer",
-							-- ["]B"] = "@block.outer",
-							-- ["]A"] = "@parameter.inner",
-						},
-						goto_previous_start = {
-							-- ["[m"] = "@function.outer",
-							-- ["gk"] = "@function.outer",
-							-- ["[["] = "@class.outer",
-							-- ["[b"] = "@block.outer",
-							-- ["[a"] = "@parameter.inner",
-						},
-						goto_previous_end = {
-							-- ["[M"] = "@function.outer",
-							-- ["gK"] = "@function.outer",
-							-- ["[]"] = "@class.outer",
-							-- ["[B"] = "@block.outer",
-							-- ["[A"] = "@parameter.inner",
-						},
-					},
-					select = {
-						enable = true,
-						lookahead = true,
-						keymaps = {
-							-- ["af"] = "@function.outer",
-							-- ["if"] = "@function.inner",
-							-- ["ac"] = "@class.outer",
-							-- ["ic"] = "@class.inner",
-							-- ["ab"] = "@block.outer",
-							-- ["ib"] = "@block.inner",
-							-- ["al"] = "@loop.outer",
-							-- ["il"] = "@loop.inner",
-							-- ["a/"] = "@comment.outer",
-							-- ["i/"] = "@comment.outer", -- no inner for comment
-							-- ["aa"] = "@parameter.outer", -- parameter -> argument
-							-- ["ia"] = "@parameter.inner",
-						},
-					},
-				},
-			})
-		end,
-	},
+	-- {
+	-- 	"nvim-treesitter/nvim-treesitter-textobjects",
+	-- 	event = "VeryLazy",
+	-- 	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	-- 	config = function()
+	-- 		require("nvim-treesitter.configs").setup({
+	-- 			textobjects = {
+	-- 				move = {
+	-- 					enable = true,
+	-- 					set_jumps = true, -- whether to set jumps in the jumplist
+	-- 					goto_next_start = {
+	-- 						-- ["]m"] = "@function.outer",
+	-- 						-- ["gj"] = "@function.outer",
+	-- 						-- ["]]"] = "@class.outer",
+	-- 						-- ["]b"] = "@block.outer",
+	-- 						-- ["]a"] = "@parameter.inner",
+	-- 					},
+	-- 					goto_next_end = {
+	-- 						-- ["]M"] = "@function.outer",
+	-- 						-- ["gJ"] = "@function.outer",
+	-- 						-- ["]["] = "@class.outer",
+	-- 						-- ["]B"] = "@block.outer",
+	-- 						-- ["]A"] = "@parameter.inner",
+	-- 					},
+	-- 					goto_previous_start = {
+	-- 						-- ["[m"] = "@function.outer",
+	-- 						-- ["gk"] = "@function.outer",
+	-- 						-- ["[["] = "@class.outer",
+	-- 						-- ["[b"] = "@block.outer",
+	-- 						-- ["[a"] = "@parameter.inner",
+	-- 					},
+	-- 					goto_previous_end = {
+	-- 						-- ["[M"] = "@function.outer",
+	-- 						-- ["gK"] = "@function.outer",
+	-- 						-- ["[]"] = "@class.outer",
+	-- 						-- ["[B"] = "@block.outer",
+	-- 						-- ["[A"] = "@parameter.inner",
+	-- 					},
+	-- 				},
+	-- 				select = {
+	-- 					enable = true,
+	-- 					lookahead = true,
+	-- 					keymaps = {
+	-- 						-- ["af"] = "@function.outer",
+	-- 						-- ["if"] = "@function.inner",
+	-- 						-- ["ac"] = "@class.outer",
+	-- 						-- ["ic"] = "@class.inner",
+	-- 						-- ["ab"] = "@block.outer",
+	-- 						-- ["ib"] = "@block.inner",
+	-- 						-- ["al"] = "@loop.outer",
+	-- 						-- ["il"] = "@loop.inner",
+	-- 						-- ["a/"] = "@comment.outer",
+	-- 						-- ["i/"] = "@comment.outer", -- no inner for comment
+	-- 						-- ["aa"] = "@parameter.outer", -- parameter -> argument
+	-- 						-- ["ia"] = "@parameter.inner",
+	-- 					},
+	-- 				},
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
 }
