751b44f5fabcae61a1d9a2872ae076db26f3328d
James Chen <jchen@cpacketnetworks.com>
2025-12-02 15:27:38 -0800

    change surround plugin

--- lua/james/plugins/mini.lua
+++ lua/james/plugins/mini.lua
@@ -1,9 +1,16 @@
 return {
-    {
-        "nvim-mini/mini.pairs",
-        version = "*",
-        config = function()
-            require('mini.pairs').setup()
-        end
-    },
+	{
+		"nvim-mini/mini.pairs",
+		version = "*",
+		config = function()
+			require("mini.pairs").setup()
+		end,
+	},
+	{
+		"nvim-mini/mini.surround",
+		version = "*",
+		config = function()
+			require("mini.surround").setup()
+		end,
+	},
 }
--- lua/james/plugins/treesitter.lua
+++ lua/james/plugins/treesitter.lua
@@ -1,83 +1,83 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "VeryLazy" },
-    lazy = vim.fn.argc(-1) == 0,
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        auto_install = true,
-        indent = { enable = true },
-        ensure_installed = { "lua", "python", "rust", "dockerfile", "yaml" },
-        sync_install = false,
-        ignore_install = { "javascript" },
-        highlight = {
-          enable = true,
-          disable = { "c" },
-          additional_vim_regex_highlighting = false,
-        },
-      })
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    event = "VeryLazy",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        textobjects = {
-          move = {
-            enable = true,
-            set_jumps = true, -- whether to set jumps in the jumplist
-            goto_next_start = {
-              -- ["]m"] = "@function.outer",
-              -- ["gj"] = "@function.outer",
-              -- ["]]"] = "@class.outer",
-              -- ["]b"] = "@block.outer",
-              -- ["]a"] = "@parameter.inner",
-            },
-            goto_next_end = {
-              -- ["]M"] = "@function.outer",
-              -- ["gJ"] = "@function.outer",
-              -- ["]["] = "@class.outer",
-              -- ["]B"] = "@block.outer",
-              -- ["]A"] = "@parameter.inner",
-            },
-            goto_previous_start = {
-              -- ["[m"] = "@function.outer",
-              -- ["gk"] = "@function.outer",
-              -- ["[["] = "@class.outer",
-              -- ["[b"] = "@block.outer",
-              -- ["[a"] = "@parameter.inner",
-            },
-            goto_previous_end = {
-              -- ["[M"] = "@function.outer",
-              -- ["gK"] = "@function.outer",
-              -- ["[]"] = "@class.outer",
-              -- ["[B"] = "@block.outer",
-              -- ["[A"] = "@parameter.inner",
-            },
-          },
-          select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-              -- ["af"] = "@function.outer",
-              -- ["if"] = "@function.inner",
-              -- ["ac"] = "@class.outer",
-              -- ["ic"] = "@class.inner",
-              -- ["ab"] = "@block.outer",
-              -- ["ib"] = "@block.inner",
-              -- ["al"] = "@loop.outer",
-              -- ["il"] = "@loop.inner",
-              -- ["a/"] = "@comment.outer",
-              -- ["i/"] = "@comment.outer", -- no inner for comment
-              -- ["aa"] = "@parameter.outer", -- parameter -> argument
-              -- ["ia"] = "@parameter.inner",
-            },
-          },
-        }
-      })
-    end
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = { "VeryLazy" },
+		lazy = vim.fn.argc(-1) == 0,
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				auto_install = true,
+				indent = { enable = true },
+				ensure_installed = { "lua", "python", "rust", "dockerfile", "yaml" },
+				sync_install = false,
+				ignore_install = { "javascript" },
+				highlight = {
+					enable = true,
+					disable = { "c" },
+					additional_vim_regex_highlighting = false,
+				},
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		event = "VeryLazy",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				textobjects = {
+					move = {
+						enable = true,
+						set_jumps = true, -- whether to set jumps in the jumplist
+						goto_next_start = {
+							-- ["]m"] = "@function.outer",
+							-- ["gj"] = "@function.outer",
+							-- ["]]"] = "@class.outer",
+							-- ["]b"] = "@block.outer",
+							-- ["]a"] = "@parameter.inner",
+						},
+						goto_next_end = {
+							-- ["]M"] = "@function.outer",
+							-- ["gJ"] = "@function.outer",
+							-- ["]["] = "@class.outer",
+							-- ["]B"] = "@block.outer",
+							-- ["]A"] = "@parameter.inner",
+						},
+						goto_previous_start = {
+							-- ["[m"] = "@function.outer",
+							-- ["gk"] = "@function.outer",
+							-- ["[["] = "@class.outer",
+							-- ["[b"] = "@block.outer",
+							-- ["[a"] = "@parameter.inner",
+						},
+						goto_previous_end = {
+							-- ["[M"] = "@function.outer",
+							-- ["gK"] = "@function.outer",
+							-- ["[]"] = "@class.outer",
+							-- ["[B"] = "@block.outer",
+							-- ["[A"] = "@parameter.inner",
+						},
+					},
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							-- ["af"] = "@function.outer",
+							-- ["if"] = "@function.inner",
+							-- ["ac"] = "@class.outer",
+							-- ["ic"] = "@class.inner",
+							-- ["ab"] = "@block.outer",
+							-- ["ib"] = "@block.inner",
+							-- ["al"] = "@loop.outer",
+							-- ["il"] = "@loop.inner",
+							-- ["a/"] = "@comment.outer",
+							-- ["i/"] = "@comment.outer", -- no inner for comment
+							-- ["aa"] = "@parameter.outer", -- parameter -> argument
+							-- ["ia"] = "@parameter.inner",
+						},
+					},
+				},
+			})
+		end,
+	},
 }
