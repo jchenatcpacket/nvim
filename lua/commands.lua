2fa73c2fdba328d2aa3c579e97b4b9039e2d3cbf
James Chen <jchen@cpacketnetworks.com>
2025-12-02 17:49:20 -0800

    yanky remap

--- lua/commands.lua
+++ lua/commands.lua
@@ -10,3 +10,55 @@ vim.api.nvim_create_user_command("Explorer", function()
 	Snacks.explorer()
 end, { desc = "File Explorer" })
 
+vim.api.nvim_create_user_command("SetClipboard", function()
+	local format_title = function(item)
+		local regtype_map = {
+			v = "charwise",
+			V = "linewise",
+		}
+		local regtype_text = regtype_map[item.regtype] or "blockwise"
+
+		if item.filetype == nil then
+			return regtype_text
+		end
+
+		return regtype_text .. " (" .. item.filetype .. ")"
+	end
+
+	Snacks.picker.pick({
+		title = "Select yanky history to system clipboard",
+		finder = function()
+			local items = {}
+			for index, value in pairs(require("yanky.history").all()) do
+				value.key = index
+				value.text = tostring(value.regcontents)
+				items[#items + 1] = value
+			end
+			return items
+		end,
+		confirm = function(picker, item)
+			picker:close()
+			if item then
+				vim.fn.setreg("+", item.text)
+			end
+			print("copied to system clipboard")
+		end,
+		format = function(item)
+			return {
+				{
+					Snacks.picker.util.align(tostring(item.idx), #tostring(item.idx), { align = "right" }),
+					"SnacksPickerIdx",
+					virtual = true,
+				},
+				{ " ", virtual = true },
+				{ item.regcontents },
+			}
+		end,
+		preview = function(ctx)
+			ctx.preview:reset()
+			ctx.preview:set_lines(ctx.item.regcontents and vim.split(ctx.item.regcontents, "\n") or {})
+			ctx.preview:set_title(format_title(ctx.item))
+			ctx.preview:highlight({ ft = ctx.item.filetype or "text" })
+		end,
+	})
+end, { desc = "Copy yanky history to system clipboard" })
--- lua/james/plugins/yanky.lua
+++ lua/james/plugins/yanky.lua
@@ -1,14 +1,10 @@
 return {
 	"gbprod/yanky.nvim",
-	opts = {},
 	dependencies = {
 		{ "folke/snacks.nvim" },
 		{ "kkharji/sqlite.lua" },
 	},
 	keys = {
-		{ "<leader>p", "<cmd>YankyRingHistory<cr>", mode = { "n", "x" }, desc = "Open Yank History" },
-		{ "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after cursor" },
-		{ "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before cursor" },
 		{
 			"<C-p>",
 			function()
@@ -25,65 +21,18 @@ return {
 				-- When pasting text from windows system clipboard, there's always a ^M at the end of each line. Adding a permeant wrapper to remove these ^M characters when pasting will allows you to paste text from windows system clipboard without any issues.
 				permanent_wrapper = require("yanky.wrappers").remove_carriage_return,
 			},
-			preserve_cursor_position = {
-				enabled = true,
-			},
-			system_clipboard = {
-				sync_with_ring = true,
-			},
 		})
 
-		 vim.api.nvim_create_user_command("SetClipboard", function()
-			local format_title = function(item)
-				local regtype_map = {
-					v = "charwise",
-					V = "linewise",
-				}
-				local regtype_text = regtype_map[item.regtype] or "blockwise"
-
-				if item.filetype == nil then
-					return regtype_text
-				end
-
-				return regtype_text .. " (" .. item.filetype .. ")"
-			end
-
-			Snacks.picker.pick({
-				title = "Select yanky history to system clipboard",
-				finder = function()
-					local items = {}
-					for index, value in pairs(require("yanky.history").all()) do
-						value.key = index
-						value.text = tostring(value.regcontents)
-						items[#items + 1] = value
-					end
-					return items
-				end,
-				confirm = function(picker, item)
-					picker:close()
-					if item then
-						vim.fn.setreg("+", item.text)
-					end
-					print("copied to system clipboard")
-				end,
-				format = function(item)
-					return {
-						{
-							Snacks.picker.util.align(tostring(item.idx), #tostring(item.idx), { align = "right" }),
-							"SnacksPickerIdx",
-							virtual = true,
-						},
-						{ " ", virtual = true },
-						{ item.regcontents },
-					}
-				end,
-				preview = function(ctx)
-					ctx.preview:reset()
-					ctx.preview:set_lines(ctx.item.regcontents and vim.split(ctx.item.regcontents, "\n") or {})
-					ctx.preview:set_title(format_title(ctx.item))
-					ctx.preview:highlight({ ft = ctx.item.filetype or "text" })
-				end,
-			})
-		end, { desc = "Copy yanky history to system clipboard" })
+		-- vim.keymap.set({ "n", "i" }, "<C-p>", function()
+		-- 	require("yanky.picker").actions.put("p") -- put after cursor
+		-- end, { desc = "yanky pick and put after cursor" })
+		-- vim.keymap.set({ "n", "i" }, "<C-P>", function()
+		--           require("yanky.picker").actions.put("P") -- put before cursor
+		-- end, { desc = "yanky pick and put before cursor" })
+		-- require("yanky.picker").actions.put("gp") -- put after cursor and leave the cursor after
+		-- require("yanky.picker").actions.put("gP") -- put before cursor and leave the cursor after
+		-- require("yanky.picker").actions.delete() -- delete entry from yank history
+		-- require("yanky.picker").actions.set_register(regname) -- fill register with selected value
+		-- require("yanky.picker").actions.put_and_set_register("p", regname) -- put and fill register with selected value
 	end,
 }
