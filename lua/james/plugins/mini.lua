ac9bcc5dea9a8be10407cbe266a9414b6ceca56b
James Chen <chenhao540@gmail.com>
2025-12-08 22:44:41 -0800

    more keymaps

--- lua/james/plugins/mini.lua
+++ lua/james/plugins/mini.lua
@@ -10,7 +10,21 @@ return {
 		"nvim-mini/mini.surround",
 		version = "*",
 		config = function()
-			require("mini.surround").setup()
+            vim.keymap.set('n', 'S', '<Nop>')
+
+			require("mini.surround").setup({
+				mappings = {
+					add = "Sa", -- Add surrounding in Normal and Visual modes
+					delete = "Sd", -- Delete surrounding
+					find = "Sf", -- Find surrounding (to the right)
+					find_left = "SF", -- Find surrounding (to the left)
+					highlwght = "Sh", -- Highlight surrounding
+					replace = "Sr", -- Replace surrounding
+
+					suffix_last = "l", -- Suffix to search with "prev" method
+					suffix_next = "n", -- Suffix to search with "next" method
+				},
+			})
 		end,
 	},
 }
