0b13d78479bcb6641188cc95fe9b521a49714555
James Chen <jchen@cpacketnetworks.com>
2025-12-10 12:07:01 -0800

    update events

--- lua/james/plugins/lint.lua
+++ lua/james/plugins/lint.lua
@@ -1,7 +1,7 @@
 return {
 	"mfussenegger/nvim-lint",
 	config = function()
-		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			callback = function()
 				-- try_lint without arguments runs the linters defined in `linters_by_ft`
 				-- for the current filetype
