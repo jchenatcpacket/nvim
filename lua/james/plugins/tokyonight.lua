04b652c9563a2d9f621a65d98afeca30e23b8e3c
James Chen <jchen@cpacketnetworks.com>
2025-12-19 12:17:58 -0800

    chaneg theme

--- lua/james/plugins/tokyonight.lua
+++ lua/james/plugins/tokyonight.lua
@@ -1,11 +1,12 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function ()
-    require("tokyonight.colors").setup({ style = "moon" })
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {},
+	config = function()
+		require("tokyonight.colors").setup({ style = "moon" })
 
-    vim.cmd.colorscheme("tokyonight")
-  end,
+		vim.cmd.colorscheme("tokyonight")
+	end,
+	enabled = false,
 }
