0c44a81667e779912f64c311e42f66a0a969a9aa
James Chen <jchen@cpacketnetworks.com>
2025-12-25 21:05:31 -0800

    remap line comment keys

--- lua/common.lua
+++ lua/common.lua
@@ -3,18 +3,20 @@ vim.opt.wrap = false
 vim.opt.cursorline = true
 vim.opt.expandtab = true
 vim.opt.autoindent = true
+-- allow external source overwrite
+vim.opt.autowriteall = true
 
 -- insert mode cursor blinking
 vim.o.guicursor = table.concat({
-  "n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
-  "i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
-  "r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100"
+	"n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+	"i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+	"r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100",
 }, ",")
 
 -- wsl clipboard
 if vim.fn.has("wsl") == 1 then
-  vim.api.nvim_exec(
-  [[
+	vim.api.nvim_exec(
+		[[
     let g:clipboard = {
     \   'name': 'win32yank-wsl',
     \   'copy': {
@@ -28,8 +30,8 @@ if vim.fn.has("wsl") == 1 then
     \   'cache_enabled': 0,
     \ }
   ]],
-  true
-)
+		true
+	)
 end
 
 if os.getenv("SSH_CLIENT") or os.getenv("SSH_TTY") then
@@ -47,18 +49,18 @@ if os.getenv("SSH_CLIENT") or os.getenv("SSH_TTY") then
 end
 
 vim.api.nvim_create_user_command("SetIndent", function(opts)
-  local width = tonumber(opts.args)
+	local width = tonumber(opts.args)
 
-  if width ~= 2 and width ~= 4 then
-    vim.api.nvim_err_writeln("Error: Indent must be either 2 or 4")
-    return
-  end
+	if width ~= 2 and width ~= 4 then
+		vim.api.nvim_err_writeln("Error: Indent must be either 2 or 4")
+		return
+	end
 
-  vim.opt.tabstop = width
-  vim.opt.softtabstop = width
-  vim.opt.shiftwidth = width
+	vim.opt.tabstop = width
+	vim.opt.softtabstop = width
+	vim.opt.shiftwidth = width
 
-  _G.indent_count = width
+	_G.indent_count = width
 end, { nargs = 1, desc = "Set Indent spaces to either 2 or 4" })
 
 vim.api.nvim_create_autocmd("VimEnter", { command = "SetIndent 4" })
@@ -67,14 +69,14 @@ vim.cmd([[match TrailingSpace /\s\+$/]])
 vim.cmd("hi TrailingSpace ctermbg=238 guibg=#4D0000")
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-    callback = function()
-        -- highlight on yank
-        vim.highlight.on_yank({timeout = 300})
+	callback = function()
+		-- highlight on yank
+		vim.highlight.on_yank({ timeout = 300 })
 
-        -- copy to system clipboard
-        local event = vim.v.event
-        if event.operator == "y" and event.regname == "" then
-            vim.fn.setreg("+", event.regcontents, event.regtype)
-        end
-    end,
+		-- copy to system clipboard
+		local event = vim.v.event
+		if event.operator == "y" and event.regname == "" then
+			vim.fn.setreg("+", event.regcontents, event.regtype)
+		end
+	end,
 })
--- lua/james/plugins/comment.lua
+++ lua/james/plugins/comment.lua
@@ -2,8 +2,12 @@ return {
 	"numToStr/Comment.nvim",
 	config = function()
 		require("Comment").setup()
-		-- windows terminal doesnt recongize <C-/> keys
+		-- windows terminal/tmux doesn't recognize <C-/> keys
 		vim.keymap.set("i", "<C-/>", function()
+			require("Comment.api").toggle.linewise.current()
+		end, { desc = "toggle comment insert mode" })
+
+		vim.keymap.set("i", "<C-l>", function()
 			require("Comment.api").toggle.linewise.current()
 		end, { desc = "toggle comment insert mode" })
 	end,
