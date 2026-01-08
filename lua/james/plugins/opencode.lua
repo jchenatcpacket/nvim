5e149af6f1dc4254d61fd6f38d96086a310790f5
James Chen <jchen@cpacketnetworks.com>
2026-01-08 15:29:27 -0800

    update opencode keymap

--- lua/james/plugins/opencode.lua
+++ lua/james/plugins/opencode.lua
@@ -17,12 +17,12 @@ return {
 
 		-- Recommended/example keymaps.
 		vim.keymap.set({ "n", "x" }, "<leader>aa", function()
-			require("opencode").ask("@this: ", { submit = true })
+			require("opencode").ask("@this: ", { submit = false })
 		end, { desc = "Ask opencode" })
 		vim.keymap.set({ "n", "x" }, "<leader>as", function()
 			require("opencode").select()
 		end, { desc = "Execute opencode action…" })
-		vim.keymap.set({ "n", "t" }, "<leader>at", function()
+		vim.keymap.set({ "n" }, "<leader>at", function()
 			require("opencode").toggle()
 		end, { desc = "Toggle opencode" })
 
