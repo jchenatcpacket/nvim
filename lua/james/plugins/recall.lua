49f04440b11b0cee8876e5edcb30cd0f64c0e8bc
James Chen <jchen@cpacketnetworks.com>
2026-01-12 17:38:47 -0800

    recall shorcuts

--- lua/james/plugins/recall.lua
+++ lua/james/plugins/recall.lua
@@ -4,9 +4,12 @@ return {
 	config = function()
 		local recall = require("recall")
 
+
 		recall.setup({})
 
 		vim.keymap.set("n", "<leader>ma", recall.toggle, { desc = "create a mark" })
+		vim.keymap.set("n", "<leader>mA", "<cmd>RecallUnmark<cr>", { desc = "unmark" })
+		vim.keymap.set("n", "<leader>mt", "<cmd>RecallToggle<cr>", { desc = "toggle a mark" })
 		vim.keymap.set("n", "]m", recall.goto_next, { desc = "jump to next mark" })
 		vim.keymap.set("n", "[m", recall.goto_prev, { desc = "jump to previous mark" })
 		vim.keymap.set("n", "<leader>mc", recall.clear, { desc = "clear all marks" })
