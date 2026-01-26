9955b5b50c2572a90c77c40190bc922054326b3b
James Chen <jchen@cpacketnetworks.com>
2026-01-26 13:32:48 -0800

    update lsp sig help

--- lua/james/plugins/signature.lua
+++ lua/james/plugins/signature.lua
@@ -1,11 +1,16 @@
 return {
 	"ray-x/lsp_signature.nvim",
 	event = "InsertEnter",
-	opts = {
-		-- cfg options
-		bind = true,
-		handler_opts = {
-			border = "rounded",
-		},
-	},
+	config = function()
+		require("lsp_signature").setup({
+			bind = true,
+			handler_opts = {
+				border = "rounded",
+			},
+		})
+
+		vim.keymap.set({ "n" }, "<C-k>", function()
+			require("lsp_signature").toggle_float_win()
+		end, { silent = true, noremap = true, desc = "toggle signature" })
+	end,
 }
