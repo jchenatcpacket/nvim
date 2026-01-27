a95827648322142a8dd24c624445a98121871674
James Chen <jchen@cpacketnetworks.com>
2026-01-26 17:28:55 -0800

    update fzf config

--- lua/james/plugins/fzf.lua
+++ lua/james/plugins/fzf.lua
@@ -1,9 +1,14 @@
 return {
 	"ibhagwan/fzf-lua",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {},
 	config = function()
 		local fzf = require("fzf-lua")
+		fzf.setup({
+			grep = {
+				-- include hidden files for grep
+				hidden = true,
+			},
+		})
 		fzf.register_ui_select()
 
 		vim.keymap.set({ "n" }, "<C-f>", ":FzfLua ", { desc = "fzflua command" })
