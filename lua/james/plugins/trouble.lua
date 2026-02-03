1145fd74ac5b6206a96b8e3a19ebeb91a1255142
James Chen <jchen@cpacketnetworks.com>
2026-02-03 11:58:28 -0800

    win config

--- lua/james/plugins/trouble.lua
+++ lua/james/plugins/trouble.lua
@@ -1,27 +1,42 @@
 return {
 	"folke/trouble.nvim",
-	opts = {}, -- for default options, refer to the configuration section for custom setup.
+	opts = {
+		win = {
+			position = "right",
+			size = 0.5,
+		},
+	},
 	cmd = "Trouble",
 	keys = {
 		{
 			"<leader>cd",
-			"<cmd>Trouble diagnostics toggle win.position=right win.size=70<cr>",
+			"<cmd>Trouble diagnostics toggle<cr>",
 			desc = "Diagnostics (Trouble)",
 		},
 		{
 			"<leader>cs",
-			"<cmd>Trouble symbols toggle focus=false win.position=right win.size=70<cr>",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
 			desc = "Symbols (Trouble)",
 		},
 		{
 			"<leader>cl",
-			"<cmd>Trouble loclist toggle win.position=right win.size=70<cr>",
+			"<cmd>Trouble loclist toggle<cr>",
 			desc = "Location List (Trouble)",
 		},
 		{
 			"<leader>cf",
-			"<cmd>Trouble qflist toggle win.position=right win.size=70<cr>",
+			"<cmd>Trouble qflist toggle<cr>",
 			desc = "Quickfix List (Trouble)",
+		},
+		{
+			"<leader>ci",
+			"<cmd>Trouble lsp_incoming_calls<cr>",
+			desc = "lsp incoming calls",
+		},
+		{
+			"<leader>co",
+			"<cmd>Trouble lsp_outgoing_calls<cr>",
+			desc = "lsp outgoing calls",
 		},
 	},
 }
