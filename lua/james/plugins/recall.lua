4e7b0812bd5544408ff922160833c11d2b0424bf
James Chen <jchen@cpacketnetworks.com>
2026-02-09 15:09:28 -0800

    update recall keymap

--- lua/james/plugins/recall.lua
+++ lua/james/plugins/recall.lua
@@ -9,7 +9,6 @@ return {
 
 		vim.keymap.set("n", "<leader>ma", recall.toggle, { desc = "toggle a mark" })
 		vim.keymap.set("n", "<leader>mA", "<cmd>RecallUnmark<cr>", { desc = "unmark" })
-		vim.keymap.set("n", "<leader>mt", "<cmd>RecallToggle<cr>", { desc = "toggle a mark" })
 		vim.keymap.set("n", "]m", recall.goto_next, { desc = "jump to next mark" })
 		vim.keymap.set("n", "[m", recall.goto_prev, { desc = "jump to previous mark" })
 		vim.keymap.set("n", "<leader>mc", recall.clear, { desc = "clear all marks" })
