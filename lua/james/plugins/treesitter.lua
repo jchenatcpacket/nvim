80f60fd5d67a303d515c6b026c844f62cb4730f0
James Chen <jchen@cpacketnetworks.com>
2026-03-12 12:56:29 -0700

    ts context config

--- lua/james/plugins/treesitter.lua
+++ lua/james/plugins/treesitter.lua
@@ -79,5 +79,8 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter-context",
 		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("treesitter-context").setup({ max_lines = 3 })
+		end,
 	},
 }
