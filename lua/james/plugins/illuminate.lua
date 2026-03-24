622bad46154578034c5f1603a5350f00fb16a26c
James Chen <jchen@cpacketnetworks.com>
2026-03-24 13:11:52 -0700

    update illuminarte hl

--- lua/james/plugins/illuminate.lua
+++ lua/james/plugins/illuminate.lua
@@ -2,5 +2,9 @@ return {
 	"RRethy/vim-illuminate",
 	config = function()
 		require("illuminate").configure({})
+
+		vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#1c1b1b" })
+		vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = "#1c1b1b" })
+		vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#1c1b1b" })
 	end,
 }
