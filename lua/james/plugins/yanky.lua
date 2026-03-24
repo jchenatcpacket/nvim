26d8830a8a5156e48d35d5d6132fa157d7688fd7
James Chen <jchen@cpacketnetworks.com>
2026-03-24 16:48:13 -0700

    rm indent

--- lua/james/plugins/yanky.lua
+++ lua/james/plugins/yanky.lua
@@ -3,15 +3,18 @@ return {
 	dependencies = {
 		{ "kkharji/sqlite.lua" },
 	},
-	opts = {
-		ring = {
-			storage = "sqlite",
-			-- permanent_wrapper = require("yanky.wrappers").remove_carriage_return,
-		},
-	},
-	keys = {
-		{ "<leader>p", "<cmd>YankyRingHistory<cr>", mode = { "n", "x" }, desc = "Open Yank History" },
-		{ "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after cursor" },
-		{ "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before cursor" },
-	},
+	config = function()
+		require("yanky").setup({
+			ring = {
+				storage = "sqlite",
+				permanent_wrapper = require("yanky.wrappers").remove_carriage_return,
+			},
+			picker = {
+				select = {
+					action = require("yanky.picker").actions.set_register('"'),
+				},
+			},
+		})
+		vim.keymap.set({ "n", "x" }, "<leader>p", "<cmd>YankyRingHistory<cr>", { desc = "Open Yank History" })
+	end,
 }
